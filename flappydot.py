7804bfef7a2ec74b1a05b401be6eddb2eac0ebc8
unknown <noppharut.k@ku.th>
2021-03-24 22:43:19 +0700

    Resolve #2

--- flappydot.py
+++ flappydot.py
@@ -8,14 +8,28 @@ CANVAS_HEIGHT = 500
 UPDATE_DELAY = 33
 GRAVITY = 2.5
 STARTING_VELOCITY = -30
+JUMP_VELOCITY = -20
 
 class Dot(Sprite):
     def init_element(self):
         self.vy = STARTING_VELOCITY
+        self.is_started = False
 
     def update(self):
-        self.y += self.vy
-        self.vy += GRAVITY
+        if self.is_started:
+            self.y += self.vy
+            self.vy += GRAVITY
+
+    def start(self):
+        self.is_started = True
+        print("Game start")
+
+    def jump(self):
+        self.vy = JUMP_VELOCITY
+
+    def is_out_of_screen(self):
+        pass
+
 
 class FlappyGame(GameApp):
     def create_sprites(self):
@@ -33,12 +47,20 @@ class FlappyGame(GameApp):
         pass
 
     def on_key_pressed(self, event):
-        pass
+        if event.char == " ":
+            if self.dot.is_started == True:
+                self.dot.jump()
+            else:
+                self.dot.start()
+
+
+
+
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("Monkey Banana Game")
+    root.title("Flappy Dot Game")
  
     # do not allow window resizing
     root.resizable(False, False)
