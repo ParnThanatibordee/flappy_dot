8c355f3d0898a11fb50aa9fe1b073765e08bafc7
ParnThanatibordee <parn8946@gmail.com>
2021-03-25 10:54:18 +0700

    Resolve #5

--- flappydot.py
+++ flappydot.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 from gamelib import Sprite, GameApp, Text
+import random
 
 CANVAS_WIDTH = 800
 CANVAS_HEIGHT = 500
@@ -8,6 +9,7 @@ CANVAS_HEIGHT = 500
 UPDATE_DELAY = 33
 GRAVITY = 2.5
 STARTING_VELOCITY = -30
+PILLAR_SPEED = 10
 
 
 class Dot(Sprite):
@@ -20,8 +22,24 @@ class Dot(Sprite):
 
 
 class PillarPair(Sprite):
+    def init_element(self):
+        self.vx = PILLAR_SPEED
+
     def update(self):
-        self.x += -10
+        if self.is_out_of_screen():
+            self.reset_position()
+        self.x -= self.vx
+
+    def is_out_of_screen(self):
+        if self.x == -40:
+            return True
+        return False
+
+    def reset_position(self):
+        self.x = CANVAS_WIDTH + 40
+
+    def random_height(self):
+        pass
 
 
 class FlappyGame(GameApp):
